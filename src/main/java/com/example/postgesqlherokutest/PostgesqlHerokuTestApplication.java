bcdcac241b07130ddd2b22ccb05c78b5741d1d56
Natália Satie <naty.odashima@gmail.com>
2022-03-25 21:04:00 -0300

    add demo spring

---
+++ src/main/java/com/example/postgesqlherokutest/PostgesqlHerokuTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgesqlherokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgesqlHerokuTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgesqlHerokuTestApplication.class, args);
+	}
+
+}
