3a6b9e8f46d0f55ba3d4adc8d44a19e15edab64a
Natália Satie <naty.odashima@gmail.com>
2022-04-01 13:48:23 -0300

    add book controller

--- src/main/java/com/example/postgesqlherokutest/controller/BookController.java
+++ src/main/java/com/example/postgesqlherokutest/controller/BookController.java
@@ -1,4 +1,65 @@
 package com.example.postgesqlherokutest.controller;
 
+import com.example.postgesqlherokutest.model.Book;
+import com.example.postgesqlherokutest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class BookController {
+    @Autowired
+    BookRepository repository;
+
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getAllBooks(@RequestParam(required = false) String title) {
+        try {
+            List<Book> list = new ArrayList<Book>();
+
+            if (title == null)
+                repository.findAll().forEach(list::add);
+            else
+                repository.findByTitleContaining(title).forEach(list::add);
+
+            if (list.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/books")
+    public ResponseEntity<Book> create(@RequestBody Book local) {
+        try {
+            Book _local = repository
+                    .save(new Book(
+                        local.getTitle(),
+                        local.getCategory(),
+                        local.getYearRelease()
+                    ));
+            return new ResponseEntity<>(_local, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/books")
+    public ResponseEntity<HttpStatus> deleteAll() {
+        try {
+            repository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
+
